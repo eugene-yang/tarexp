90ad20afe8bd8876432a0ee487ac90645247f354
Eugene Yang <eugene2528@gmail.com>
2022-02-13 23:50:01 -0500

    SuccessProbLabeler

--- tarexp/component/labeler.py
+++ tarexp/component/labeler.py
@@ -22,3 +22,20 @@ class PerfectLabeler(Labeler):
     
     def labelDocs(self, doc_ids, **kwargs):
         return list(self._Y[doc_ids])
+
+class SuccessProbLabeler(Labeler):
+    def __init__(self, success_prob=0.5, **kwargs):
+        super().__init__()
+        self._success_prob = success_prob
+        self.reset()
+    
+    def begin(self, dataset: Dataset, **kwargs):
+        self._Y = np.asanyarray(dataset.labels)
+    
+    def reset(self):
+        self._Y = None
+    
+    def labelDocs(self, doc_ids, random: np.random.RandomState, **kwargs):
+        labs = self._Y[doc_ids]
+        flip = random.uniform(size=len(labs)) > self._success_prob
+        return list(np.bitwise_xor(labs, flip))
